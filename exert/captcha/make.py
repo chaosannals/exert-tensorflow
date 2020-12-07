11e85eb59ab16ac6479f3f0e37f6bee7be6253a2
chaosannals <chaosannals@outlook.com>
2020-12-07 15:12:07 +0800

    log

--- exert/captcha/make.py
+++ exert/captcha/make.py
@@ -5,20 +5,16 @@ from importlib import import_module
 from loguru import logger
 Captcha = import_module('data').Captcha
 
-def random_text():
-    '''
-    '''
-
 
 if __name__ == '__main__':
     captcha = Captcha()
     d = 'captcha'
     if not os.path.isdir(d):
         os.makedirs(d, 0o777, True)
-    for i in range(100):
+    for i in range(100000):
         t = math.floor(time.time())
         c = captcha.roll_text()
         p = f'{d}/{t}-{c}.png'
         captcha.save(c, p)
-        logger.info(f'{c} => {p}')
+        logger.info(f'[{i:06}] {c} => {p}')
 
